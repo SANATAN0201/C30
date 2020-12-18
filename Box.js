a1aec2cc104c0353a9587119d239cdab6c0400db
SANATAN0201 <71132509+SANATAN0201@users.noreply.github.com>
2020-12-18 14:09:31 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -24,6 +24,13 @@ class Box {
     rect(0, 0, this.width, this.height);
     pop();
   }
+   else{
+    World.remove(world, this.body);
+    push()
+    this.visibility = this.visibility - 5 
+    tint(255,this.visibility)
+    pop()
 
+   }
   }
 }
